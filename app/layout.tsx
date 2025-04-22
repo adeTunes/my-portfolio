ff4a155b43a48e72310ef8cab06a449c593144d9
Adekunle Babatunde <babatundeadebowale1998@gmail.com>
2025-04-22 02:51:43 +0100

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     },
     { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-icon.png" },
   ],
-  manifest: "/site.webmanifest"
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -60,6 +60,24 @@ export default function RootLayout({
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={poppins.className}>
         <MantineProvider theme={theme}>{children}</MantineProvider>
